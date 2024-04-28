b029073b4fb06c5997a984dec6819bb37a717ff9
Maciek Laptop <moroz.maciek@gmai.com>
2024-04-28 13:59:40 +0200

    zadanie 2

--- src/main/Card.java
+++ src/main/Card.java
@@ -1,4 +1,65 @@
 package main;
 
 public class Card {
+    public enum Suit {
+        CLUBS, DIAMONDS, HEARTS, SPADES
+    }
+    private Suit suit;
+    private int value;
+    private String[] ranks = {"Jack", "Queen", "King", "Ace"};
+    private String rank;
+    public Card(Suit suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
+    public Card(Suit suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+        this.value = this.getRankValue();
+    }
+    public Card(){}
+    public Suit getSuit() {
+        return suit;
+    }
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+    public int getValue() {
+        return value;
+    }
+    public void setValue(int value) {
+        this.value = value;
+    }
+    public String getRank() {
+        return rank;
+    }
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+    public int getRankValue() {
+        for (int i = 0; i < ranks.length; i++){
+            if (ranks[i].equals(rank)){
+                return i+11;
+            }
+        }
+        return 0;
+    }
+    private int Compare(Card card, Card otherCard){
+        if(card.getValue() > otherCard.getValue()){
+            return 1;
+        } else if (card.getValue() < otherCard.getValue()) {
+            return 2;
+        }else{
+            return 0;
+        }
+    }
+    public String announceCompare(Card card, Card otherCard){
+        return switch (Compare(card, otherCard)) {
+            case 1 -> "Karta pierwsza przebija drugą!";
+            case 2 -> "Karta druga przebija pierwszą!";
+            case 0 -> "Karty są sobie równe!";
+            default -> "Error!";
+        };
+
+    }
 }
--- src/test/Numbers_Test.java
+++ src/test/Numbers_Test.java
@@ -2,7 +2,6 @@ package test;
 
 import main.Numbers;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 
